e389d08f6adce68c779ba6979cfbaba9dedb5122
@yrauvtovich <rama7777@bk.ru>
2020-07-21 08:25:05 +0300

    Update code 21/07/2020

--- src/script.js
+++ src/script.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
     $('#nav-icon').click(function(){
-        $(this).toggleClass('open');
+        $(this).toggleClass('open', 'triggered');
+    });
+    $('.has-children > a').after('<span></span>');
+    $('.has-children span').on('click', function() {
+        $(this).toggleClass('subtriggered');
     });
 });
+
