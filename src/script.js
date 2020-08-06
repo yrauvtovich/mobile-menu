1c0b4fb38b6f8af2acb4a19a8eb14e7a161183c7
@yrauvtovich <rama7777@bk.ru>
2020-08-06 10:02:42 +0300

    Update code 06/08/2020

--- src/script.js
+++ src/script.js
@@ -3,10 +3,11 @@ $(document).ready(function(){
         $(this).toggleClass('open');
         $('.mobile-menu').toggleClass('triggered');
     });
-
     $('.has-children > a').after('<span></span>');
     $('.has-children span').on('click', function() {
         $(this).toggleClass('subtriggered');
     });
+
+
 });
 
