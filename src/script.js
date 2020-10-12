f8cbed4e31cc0eca800e035e6e917f4798c0921f
Yauhen Rauvtovich <rama7777@bk.ru>
2020-10-12 15:42:35 +0300

    Update code 12/10/2020

--- src/script.js
+++ src/script.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
-    $('#nav-icon').click(function(){
-        if(!$(this).hasClass("open")){
-            $(this).toggleClass('open');
-            $('.mobile-menu').toggleClass('triggered');
-            $ ('div.menu').show();
-        } else {
-            $(this).removeClass('open');
-            $('.mobile-menu').removeClass('triggered');
-            $('div.menu').hide();
-        }
-    });
-    $('div.menu').hide();
+  $('#nav-icon').click(function(){
+    if(!$(this).hasClass("open")){
+      $(this).toggleClass('open');
+      $('.mobile-menu').toggleClass('triggered');
+      $ ('div.menu').show();
+    } else {
+      $(this).removeClass('open');
+      $('.mobile-menu').removeClass('triggered');
+      $('div.menu').hide();
+    }
+  });
+  $('div.menu').hide();
 });
 
