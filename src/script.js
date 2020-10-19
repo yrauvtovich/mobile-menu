964800a6e7554aaebe1fdd194296f8e3bea85178
Yauhen Rauvtovich <rama7777@bk.ru>
2020-10-19 15:39:57 +0300

    Update code 19/10/2020

--- src/script.js
+++ src/script.js
@@ -3,13 +3,14 @@ $(document).ready(function(){
     if(!$(this).hasClass("open")){
       $(this).toggleClass('open');
       $('.mobile-menu').toggleClass('triggered');
-      $ ('div.menu').show();
+      $('div.menu').show();
+      $('div.menu ul li').show();
     } else {
       $(this).removeClass('open');
       $('.mobile-menu').removeClass('triggered');
       $('div.menu').hide();
+      $('div.menu ul li').hide();
     }
   });
-  $('div.menu').hide();
 });
 
